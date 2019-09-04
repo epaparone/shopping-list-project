30dcb2b7bda6375e35c96a251b6f6ac9da3c45bc
paparoneelizabeth@gmail.com <Elizabethpaparone@Elizabeths-MBP.lan1>
2019-09-04 16:02:28 -0400

    added checkItem & deleteItem functions

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-// I have an idea of what I need to do
-// First I need to say that this code can only execute 
-// once the page is fully loaded
 function appendList() {
-    // I need to find the form element and tell it to do something
-    // when a user submits an input
     $('#js-shopping-list-form').submit(function( event ) {
-        // Then I need to prevent the default form behavior
         event.preventDefault();
         let inputVal = $('#shopping-list-entry').val();
         console.log(inputVal);
-        // Next I need to tell it to take the input and add it to 
-        // the end of the shopping list - using .append
-        $('.shopping-list').append(`<li><span class="shopping-item">${inputVal}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div></li>`);
-        });
-    }
-
-function checkItem() {};
-function deleteItem() {};
-
-function initJQuery() {
-    checkItem();
-    deleteItem();
-    appendList();
+        $('.shopping-list').append(`<li>
+            <span class="shopping-item">${inputVal}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+            </li>`);
+    });
 };
 
-$(initJQuery);
+function checkItem() {
+    $('.shopping-item-toggle').click(function() {
+        $(this).closest('div').prev('span').toggleClass('shopping-item__checked');
+    });
+};
+
+function deleteItem() {
+    $('.shopping-item-delete').click(function() {
+        $(this).closest('li').remove();
+    });
+};
+
+function initJQuery() {
+    appendList();
+    checkItem();
+    deleteItem();
+};
 
 
-  
+$(initJQuery)
